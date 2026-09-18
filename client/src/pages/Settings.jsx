1b44610c64d0a7a9a054e43b3524edeb7c46af30
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2026-09-18 23:37:18 +0530

    Change Password Modal added

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -3,6 +3,7 @@ import { dummyProfileData } from "../assets/assets"
 import Loading  from "../components/Loading"
 import { Lock } from "lucide-react"
 import ProfileForm from "../components/ProfileForm"
+import ChangePasswordModal from "../components/ChangePasswordModal"
 
 
 const Settings = () => {
@@ -54,6 +55,8 @@ const Settings = () => {
           Change
         </button>
       </div>
+
+      <ChangePasswordModal open={showPasswordModal} onClose={() => setShowPasswordModal(false)} />
     </div>
   )
 }
